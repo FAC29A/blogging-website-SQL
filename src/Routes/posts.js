7b3a8394063bc4fc3bd286b58930367a3702353d
Lucien Lin <luciensday@gmail.com>
2024-02-06 21:19:44 +0000

    seed is display on website now

--- src/Routes/posts.js
+++ src/Routes/posts.js
@@ -3,74 +3,25 @@ const router = express.Router();
 const helper = require("../helper");
 const model = require("../../model/blogs")
 
-const errorsObject = {};
+const errorsObject = {}; // declare it globally so all render methods are able to pass it as an argument. Empty object serve as default purpose
 
-
-//old server route
-// router.get("/", (request, response) => {
-//   const requestBody = {};
-//   response.render("posts", {
-//     title: "posts",
-//     blogPosts,
-//     errorsObject,
-//     requestBody,
-//     helper,
-//   });
-// });
-
-//new server route- need to finish working this out!!
 router.get("/", (request, response) => {
   const blogs = model.displayBlogs();
-  const requestBody = {};
-  console.log(blogs)
+  const requestBody = {}; // no actual utility in this block- declare it in order to pass into render method
   
   response.render("posts", {
     title: "posts",
-    errorsObject,
-    requestBody,
+    errorsObject, // no actual utility in this block but mandatory to pass it as part of render method constraint (otherwise ejs file won't find variable)
+    requestBody, // no actual utility in this block but mandatory to pass it as part of render method constraint (otherwise ejs file won't find variable)
     helper,
     blogs
   });
 });
 
-//old server route
-// router.post("/", (request, response) => {
-//   const requestBody = request.body;
-  // const name = request.body.name;
-  // const blogpost = request.body.blogpost;
-
-  //validation and error handling
-  // const errorsObject = {};
-  // if (!name) {
-  //   errorsObject.nameError = "please enter your name";
-  // }
-  // if (!blogpost) {
-  //   errorsObject.postError = "please enter a message";
-  // }
-
-  // if (Object.keys(errorsObject).length > 0) {
-  //   response.render("posts", {
-  //     title: "posts",
-  //     blogPosts,
-  //     errorsObject,
-  //     requestBody,
-  //     helper,
-  //   });
-  // } else {
-  //   const date = new Date();
-  //   let displayDate = date.toDateString();
-  //   const postId = date.getTime();
-  //   blogPosts.push({ name, blogpost, postId, displayDate });
-  //response.redirect("/posts");
-  // }
-// });
-
-//new server route
 router.post("/", (request, response) => {
   const requestBody = request.body;
   const name = request.body.name;
   const blogpost = request.body.blogpost;
-
 
   //validation and error handling
   const errorsObject = {};
@@ -95,21 +46,16 @@ router.post("/", (request, response) => {
     name: name,
     blogpost: blogpost
   }
-
   model.createBlog(blogEntry);
   response.redirect("/posts");
 }
 });
 
 
-
-
 router.post("/delete/:id", (req, res) => {
   const postId = Number(req.params.id);
-  console.log(postId);
   model.deleteTask(postId);
   res.redirect("/posts");
-  
 });
 
 module.exports = router;
