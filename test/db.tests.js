88532b7f896abeca9e9f651d0504c7f50eaabe60
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2024-02-06 15:08:19 +0000

    Created first test to add blog post

---
+++ test/db.tests.js
@@ -0,0 +1,28 @@
+const test = require("node:test");
+const assert = require("node:assert");
+const model = require("../model/blogs.js")
+const db = require("../database/db.js")
+
+function reset() {
+    db.exec(/*sql*/`
+    DELETE FROM blog_posts;
+    DELETE FROM sqlite_sequence WHERE name = "name";
+    `)
+}
+
+test("can create a blog post", () => {
+    reset()
+
+    const post = model.createBlog({ name: "test", blogpost: "testing" })
+    assert.equal(post.name, "test")
+    assert.equal(post.id, 1)
+    assert.equal(post.blogpost, "testing")
+})
+
+test("can create a blog post", () => {
+    reset()
+
+    const post = model.createBlog({ name: "test", blogpost: "testing" })
+    assert.equal(post.name, "test")
+    assert.equal(post.blogpost, "testing")
+})
